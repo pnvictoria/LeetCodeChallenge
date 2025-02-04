1ed444bae60e7b05933f0ba0ffe963a02a050289
pnvictoria <121459272+pnvictoria@users.noreply.github.com>
2025-02-04 19:43:52 +0200

    Time: 3 ms (90.14%), Space: 44.6 MB (45.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,11 +4,11 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         char[] arrS = s.toCharArray();
         char[] arrT = t.toCharArray();
-        Arrays.sort(arrS);
-        Arrays.sort(arrT);
         if (arrS.length != arrT.length) {
             return false;
         }
+        Arrays.sort(arrS);
+        Arrays.sort(arrT);
         return Arrays.equals(arrS, arrT);
     }
 }
